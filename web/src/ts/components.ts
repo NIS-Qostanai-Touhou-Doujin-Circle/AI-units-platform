c4e4efb214bdc3c8560472527c5f4740267f4762
omgaXD <sladekop@gmail.com>
2025-08-26 14:00:03 +0500

    add components page

--- web/src/ts/components.ts
+++ web/src/ts/components.ts
@@ -1,7 +1,7 @@
 import Chart from "chart.js/auto";
 import colors from "tailwindcss/colors";
 
-export function helloWorld() {
+export function pieChart() {
 	const canvas = document.getElementById('pie-chart') as HTMLCanvasElement;
 	const ctx = canvas.getContext("2d");
 	if (!ctx) return;
@@ -28,4 +28,66 @@ export function helloWorld() {
 	});
 }
 
-helloWorld();
+function barChart() {
+	const canvas = document.getElementById('bar-chart') as HTMLCanvasElement;
+	const ctx = canvas.getContext("2d");
+	if (!ctx) return;
+
+	const chart = new Chart(ctx, {
+		type: "bar",
+		options: {
+			plugins: {
+				title: {
+					text: "Hello World Chart",
+					display: true,
+				},
+				legend: {
+					display: false
+				}
+			}
+		},
+		data: {
+			labels: ["Hello", "World!"],
+			datasets: [
+				{
+					data: [5, 6],
+					backgroundColor: [colors.red[500], colors.blue[500]],
+				},
+			],
+		},
+	});
+}
+
+function lineChart() {
+	const canvas = document.getElementById('line-chart') as HTMLCanvasElement;
+	const ctx = canvas.getContext("2d");
+	if (!ctx) return;
+
+	const chart = new Chart(ctx, {
+		type: "line",
+		options: {
+			plugins: {
+				title: {
+					text: "Hello World Chart",
+					display: true,
+				},
+				legend: {
+					display: false
+				}
+			},
+		},
+		data: {
+			labels: ["Hello", "The", "World!"],
+			datasets: [
+				{
+					data: [5, 8, 6],
+					backgroundColor: [colors.red[500], colors.green[500], colors.blue[500]],
+				},
+			],
+		},
+	});
+}
+
+pieChart();
+barChart();
+lineChart();
